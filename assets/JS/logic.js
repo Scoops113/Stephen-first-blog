eaab8c9027839097ba2494cb119bf6453167d013
Stephen <stephencooper11395@gmail.com>
2024-03-18 14:12:30 -0500

    added Javascript to blog and made edits to CSS

---
+++ assets/JS/logic.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleModeButton = document.getElementById('toggleMode');
+    const body = document.body;
+
+    toggleModeButton.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+        body.classList.toggle('light-mode');
+    });
+});
